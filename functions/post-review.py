378e2a447febbf4977cbb3ca68177a363a30b63e
YotamGit <yotam.gol@gmail.com>
2022-08-18 16:38:36 +0300

    add action code

---
+++ functions/post-review.py
@@ -0,0 +1,31 @@
+#
+#
+# main() will be run when you invoke this action
+#
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+#
+# @return The output of this action, which must be a JSON object.
+#
+#
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+
+def main(dict):
+    authenticator = IAMAuthenticator(dict['IAM_API_KEY'])
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url(dict['COUCH_URL')
+    response= service.post_document(db='reviews', document=dict["review"]).get_result()
+    try:
+    # result_by_filter=my_database.get_query_result(selector,raw_result=True)
+        result = {
+        'headers': {'Content-Type': 'application/json'},
+        'body': {'data': response}
+        }
+        return result
+    except:
+        return {
+        'statusCode': 404,
+        'message': 'Something went wrong'
+        }
